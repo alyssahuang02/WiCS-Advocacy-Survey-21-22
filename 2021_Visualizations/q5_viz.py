cf4d46fba789db92c49a02e5465875375a9221b0
Alyssa <alyssahuang@college.harvard.edu>
2022-03-06 12:19:36 -0500

    fixed formatting for all except 22 and 23

--- 2021_Visualizations/q5_viz.py
+++ 2021_Visualizations/q5_viz.py
@@ -17,7 +17,7 @@ import dash_bootstrap_components as dbc
 
 # constants
 # column titles with formatting
-COLUMN_TITLES = ['', 'My first-year <br> academic advisor <br> <br>', 'Residential advising <br> resources, such as <br> house tutors <br> or proctors <br> <br>',
+COLUMN_TITLES = ['', 'My first-year <br> academic advisor <br> <br>', 'Residential advising <br> Resources, such as <br> house tutors <br> or proctors <br> <br>',
                  'Department <br> heads <br> <br>', 'Department <br> faculty <br> <br>', 'Department-specific <br> advising programs <br> <br>', 
                  'Peer <br> Advising <br> Fellows (PAFs) <br> <br>']
 
@@ -49,113 +49,6 @@ app.layout = html.Div([
             )
         ])
     ], style={'width': '20%', 'display': 'inline-table', 'margin-top': 20, 'margin-left': 50}),
-    html.Div([
-        html.H4('Filter'),
-        dbc.Button("Select Filters", color="info",
-                   id="open-filter", className="mr-1"),
-        dbc.Modal(
-            [
-                dbc.ModalHeader("Filter Options"),
-                dbc.ModalBody(
-                    "You may only select up to two filters at a time, and you may not filter on the basis of your selected split."),
-                html.Div([
-                    html.Div([
-                        html.P('Gender'),
-                        dcc.Dropdown(
-                            id='filter-gender-dropdown',
-                            options=[{'label': i, 'value': i}
-                                     for i in C.GENDER_FILTER_OPTIONS],
-                            value='All',
-                            clearable=False
-                        )
-                    ],
-                        style={'width': '40%', 'display': 'inline-block', 'margin': 15}),
-                    html.Div([
-                        html.P('Race/Ethnicity'),
-                        dcc.Dropdown(
-                            id='filter-race-ethnicity-dropdown',
-                            options=[{'label': i, 'value': i}
-                                     for i in C.RACE_ETHNICITY_FILTER_OPTIONS],
-                            value='All',
-                            clearable=False
-                        )
-                    ],
-                        style={'width': '40%', 'display': 'inline-block', 'margin': 15})
-                ]),
-                html.Div([
-                    html.Div([
-                        html.P('BGLTQ+'),
-                        dcc.Dropdown(
-                            id='filter-bgltq-dropdown',
-                            options=[{'label': i, 'value': i}
-                                     for i in C.BGLTQ_FILTER_OPTIONS],
-                            value='All',
-                            clearable=False
-                        )
-                    ],
-                        style={'width': '40%', 'display': 'inline-block', 'margin': 15}),
-                    html.Div([
-                        html.P('First Generation, Low Income (FGLI)'),
-                        dcc.Dropdown(
-                            id='filter-fgli-dropdown',
-                            options=[{'label': i, 'value': i}
-                                     for i in C.FGLI_FILTER_OPTIONS],
-                            value='All',
-                            clearable=False
-                        )
-                    ],
-                        style={'width': '40%', 'display': 'inline-block', 'margin': 15}),
-                ]),
-                html.Div([
-                    html.Div([
-                        html.P('Class Year'),
-                        dcc.Dropdown(
-                            id='filter-class-year-dropdown',
-                            options=[{'label': i, 'value': i}
-                                     for i in C.CLASS_YEAR_FILTER_OPTIONS],
-                            value='All',
-                            clearable=False
-                        )
-                    ],
-                        style={'width': '40%', 'display': 'inline-block', 'margin': 15}),
-                    html.Div([
-                        html.P('School of Primary Concentration'),
-                        dcc.Dropdown(
-                            id='filter-school-dropdown',
-                            options=[{'label': i, 'value': i}
-                                     for i in C.SCHOOL_FILTER_OPTIONS],
-                            value='All',
-                            clearable=False
-                        )
-                    ],
-                        style={'width': '40%', 'display': 'inline-block', 'margin': 15})
-                ]),
-                html.Div([
-                    html.Div([
-                        html.P('Primary Concentration'),
-                        dcc.Dropdown(
-                            id='filter-concentration-dropdown',
-                            options=[{'label': i, 'value': i}
-                                     for i in C.CONCENTRATION_FILTER_OPTIONS],
-                            value='All',
-                            clearable=False
-                        )
-                    ],
-                        style={'width': '40%', 'display': 'inline-block', 'margin': 15})
-                ]),
-                dbc.ModalFooter(
-                    dbc.Button("Close", id="close-filter", className="ml-auto")
-                ),
-            ],
-            id="filter-modal",
-            size="lg",
-        ),
-        html.P('Filters: None', id='filters-label',
-               style={'font-style': 'italic'})
-    ], style={'width': '30%', 'display': 'inline-table', 'margin-top': 20, 'margin-left': 50}),
-
-
-
     html.Div(
         children=[
             dcc.Graph(id='visualization')
@@ -174,118 +67,12 @@ def is_sample_size_insufficient(dff, axis):
     # check number of responses per category is greater than minimum sample size
     return any(c < C.MIN_SAMPLE_SIZE for c in category_counts)
 
-
-@app.callback(
-    Output('filter-modal', 'is_open'),
-    Input('open-filter', 'n_clicks'),
-    Input('close-filter', 'n_clicks'),
-    State('filter-modal', 'is_open'))
-def toggle_modal(n1, n2, is_open):
-    if n1 or n2:
-        return not is_open
-    return is_open
-
-
-@app.callback(
-    Output('filter-gender-dropdown', 'disabled'),
-    Output('filter-race-ethnicity-dropdown', 'disabled'),
-    Output('filter-bgltq-dropdown', 'disabled'),
-    Output('filter-fgli-dropdown', 'disabled'),
-    Output('filter-class-year-dropdown', 'disabled'),
-    Output('filter-school-dropdown', 'disabled'),
-    Output('filter-concentration-dropdown', 'disabled'),
-    Input('axis', 'value'),
-    Input('filter-gender-dropdown', 'value'),
-    Input('filter-race-ethnicity-dropdown', 'value'),
-    Input('filter-bgltq-dropdown', 'value'),
-    Input('filter-fgli-dropdown', 'value'),
-    Input('filter-class-year-dropdown', 'value'),
-    Input('filter-school-dropdown', 'value'),
-    Input('filter-concentration-dropdown', 'value'))
-def toggle_filters(axis, gender_filter, race_ethnicity_filter, bgltq_filter, fgli_filter,
-                   class_year_filter, school_filter, concentration_filter):
-    filter_count = 0
-    filter_enable_list = [False, False, False, False, False, False, False]
-    filter_enable_list[C.VIZ_AXES.index(axis)] = True
-    filter_disable_list = filter_enable_list.copy()
-
-    filter_selections = [gender_filter, race_ethnicity_filter, bgltq_filter,
-                         fgli_filter, class_year_filter, school_filter, concentration_filter]
-    for i in range(len(filter_selections)):
-        if filter_selections[i] == 'All':
-            filter_disable_list[i] = (True)
-        else:
-            filter_count += 1
-
-    if filter_count >= 2:
-        return filter_disable_list
-    return filter_enable_list
-
-
-@app.callback(
-    Output('filter-gender-dropdown', 'value'),
-    Output('filter-race-ethnicity-dropdown', 'value'),
-    Output('filter-bgltq-dropdown', 'value'),
-    Output('filter-fgli-dropdown', 'value'),
-    Output('filter-class-year-dropdown', 'value'),
-    Output('filter-school-dropdown', 'value'),
-    Output('filter-concentration-dropdown', 'value'),
-    Input('axis', 'value'))
-def reset_filters(axis):
-    return ['All', 'All', 'All', 'All', 'All', 'All', 'All']
-
-
-@app.callback(
-    Output('filters-label', 'children'),
-    Input('filter-gender-dropdown', 'value'),
-    Input('filter-race-ethnicity-dropdown', 'value'),
-    Input('filter-bgltq-dropdown', 'value'),
-    Input('filter-fgli-dropdown', 'value'),
-    Input('filter-class-year-dropdown', 'value'),
-    Input('filter-school-dropdown', 'value'),
-    Input('filter-concentration-dropdown', 'value'))
-def set_filters_label(gender_filter, race_ethnicity_filter, bgltq_filter, fgli_filter,
-                      class_year_filter, school_filter, concentration_filter):
-    filter_str_list = []
-    filter_selections = [gender_filter, race_ethnicity_filter, bgltq_filter,
-                         fgli_filter, class_year_filter, school_filter, concentration_filter]
-    for sel in filter_selections:
-        if sel != 'All':
-            filter_str_list.append(sel)
-
-    if len(filter_str_list) == 0:
-        return 'Filters: None'
-    return 'Filters: ' + ', '.join(filter_str_list)
-
-
-def filter_df(df, gender_filter, race_ethnicity_filter, bgltq_filter, fgli_filter,
-              class_year_filter, school_filter, concentration_filter):
-    df = D.filter_gender(df, gender_filter)
-    df = D.filter_race_ethnicity(df, race_ethnicity_filter)
-    df = D.filter_bgltq(df, bgltq_filter)
-    df = D.filter_fgli(df, fgli_filter)
-    df = D.filter_class_year(df, class_year_filter)
-    df = D.filter_school(df, school_filter)
-    df = D.filter_conc(df, concentration_filter)
-    return df
-
-
 @app.callback(
     Output('visualization', 'figure'),
-    Input('axis', 'value'),
-    Input('filter-gender-dropdown', 'value'),
-    Input('filter-race-ethnicity-dropdown', 'value'),
-    Input('filter-bgltq-dropdown', 'value'),
-    Input('filter-fgli-dropdown', 'value'),
-    Input('filter-class-year-dropdown', 'value'),
-    Input('filter-school-dropdown', 'value'),
-    Input('filter-concentration-dropdown', 'value'))
-def update_graph(axis, gender_filter, race_ethnicity_filter, bgltq_filter, fgli_filter,
-                 class_year_filter, school_filter, concentration_filter):
-
+    Input('axis', 'value'))
+def update_graph(axis):
     # extract relevant data
-    dff = filter_df(D.AXIS_DF[axis], gender_filter, race_ethnicity_filter, bgltq_filter,
-                    fgli_filter, class_year_filter, school_filter, concentration_filter)
+    dff = D.AXIS_DF[axis]
     columnOptions = []
     for choice in ANSWER_OPTIONS:
         columnOptions.append(dff[dff['Q5'].str.contains(
@@ -303,15 +90,12 @@ def update_graph(axis, gender_filter, race_ethnicity_filter, bgltq_filter, fgli_
 
     for label in names:
         colNum = 1
-        # add row title
         figSub.add_trace(go.Table(
-            header=dict(values=[label], fill_color='rgba(0,0,0,0)', font=dict(
-                color='RebeccaPurple', size=16), align='center'),
+            header=dict(values=[label], fill_color='rgba(0,0,0,0)'),
             cells=dict(values=[' '],
                        fill_color='rgba(0,0,0,0)',
                        align='center')
-        ),
-            row=rowNum, col=colNum)
+        ), row=rowNum, col=colNum)
         colNum += 1
 
         # construct pie chart
@@ -334,19 +118,26 @@ def update_graph(axis, gender_filter, race_ethnicity_filter, bgltq_filter, fgli_
 
             colNum += 1
         rowNum += 1
-        print(total)
+        # print(total)
 
+    text_annotations=[]
+    text_annotations.append(dict(font=dict(size=14)))
     # check for errors
     if figSub == None or is_sample_size_insufficient(dff, axis):
         print("ERROR")
         return C.EMPTY_FIGURE
     # plot titles
     figSub.update_layout(
-        title='',
-        font=dict(
-            family="Courier New, monospace",
-            size=15,
-            color="RebeccaPurple"
+        annotations=text_annotations,
+        height=300,
+        margin=dict(l=0, r=0, t=70, b=30),
+        legend=dict(
+            yanchor="bottom",
+            y=1.02,
+            xanchor="left",
+            x=1.02,
+            itemclick=False,
+            itemdoubleclick=False
         )
     )
     return figSub
